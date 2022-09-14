62e37334fead0c6330f9ad28a93dac924368c625
neilson <joseneilson25@gmail.com>
2022-09-14 20:11:51 -0300

    components footer e title criados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FullBannerComponent } from './components/full-banner/full-banner.component';
 import { ListTitlesComponent } from './components/list-titles/list-titles.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TitleComponent } from './components/title/title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     FullBannerComponent,
-    ListTitlesComponent
+    ListTitlesComponent,
+    FooterComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule
